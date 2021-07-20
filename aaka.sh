b8766d33924d2ac30a776711b34fba93c9598390
aakashsadekar <12764878+aakashsadekar@users.noreply.github.com>
2021-07-20 08:24:45 +0530

    added fira code and jetbrains mono

--- aaka.sh
+++ aaka.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Fira Code
+sudo apt install fonts-firacode -y
+# MacOS theme
 cd ~
 mkdir ~/.icons
 mkdir ~/.themes
@@ -10,9 +13,19 @@ mv Mojave-dark-solid ~/.themes
 mv OSX-ElCap ~/.icons
 mv SanFranciscoFont-master ~/.fonts
 rm macubuntu.tar.gz
+rm -rfv didactic-funicular
+# Source Code Pro
 cd ~
 wget https://github.com/downloads/adobe/Source-Code-Pro/SourceCodePro_FontsOnly-1.009.zip \
     && unzip SourceCodePro_FontsOnly-1.009.zip \
     && sudo mkdir /usr/share/fonts/truetype/source-code-pro \
     && sudo cp SourceCodePro_FontsOnly-1.009/*.ttf /usr/share/fonts/truetype/source-code-pro
+rm SourceCodePro_FontsOnly-1.009.zip
+rm -rfv SourceCodePro_FontsOnly-1.009
 cd ~
+# JetBrains Mono
+wget https://download.jetbrains.com/fonts/JetBrainsMono-2.225.zip
+sudo unzip -d /usr/share/fonts JetBrainsMono-2.225.zip
+rm JetBrainsMono-2.225.zip
+# Refresh Font cache
+fc-cache -f -v
